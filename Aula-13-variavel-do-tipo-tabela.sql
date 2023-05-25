5b18d496e09caea784b25f221622c9d1b49baacf
Mateus Schverz <mate_fs@hotmail.com>
2023-05-24 23:55:29 -0300

    Create Aula-13-variavel-do-tipo-tabela.sql

---
+++ Aula-13-variavel-do-tipo-tabela.sql
@@ -0,0 +1,16 @@
+
+-- só funciona no escopo de execução
+-- executando separado não irá funcionar.
+
+-- criando uma tabela dentro de uma variável.
+declare @tabela table (
+	codigoProduto int primary key,
+	nomeProduto varchar(50)
+)
+
+insert into @tabela values (1,'Celular');
+select * from @tabela;
+ 
+
+
+
